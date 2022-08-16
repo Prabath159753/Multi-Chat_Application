9b798019189c0a7a59cd6425f89221c360e723f5
Kavishka <kavishkaprabath21@gmail.com>
2022-08-16 10:11:05 +0530

    update Congratulations

--- User-Interface Multi-Chat_Application/src/Client_Side/controller/MessageEnvironmentFormController.java
+++ User-Interface Multi-Chat_Application/src/Client_Side/controller/MessageEnvironmentFormController.java
@@ -1,4 +1,16 @@
 package Client_Side.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class MessageEnvironmentFormController {
+    
+    public Label lblContactName;
+    public TextArea txtArea;
+    public TextField txtMessage;
+
+    public void txtMsgOnAction(ActionEvent actionEvent) {
+    }
 }
