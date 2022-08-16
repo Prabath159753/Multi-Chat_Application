0b2acf39bad91c6860ec4f738e1eb762a425284c
Kavishka <kavishkaprabath21@gmail.com>
2022-08-16 09:41:36 +0530

    add client loginform

---
+++ User-Interface Multi-Chat_Application/src/Client_Side/controller/ClientLoginFormController.java
@@ -0,0 +1,14 @@
+package Client_Side.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientLoginFormController {
+
+    public AnchorPane mainRoot;
+    public TextField txtName;
+
+    public void continueBtnOnAction(ActionEvent actionEvent) {
+    }
+}
