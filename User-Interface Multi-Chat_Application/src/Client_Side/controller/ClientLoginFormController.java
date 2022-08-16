7e3548799b9cccb4e9790ca2368fc832310137dd
Kavishka <kavishkaprabath21@gmail.com>
2022-08-16 09:46:21 +0530

    update ClientLoginFormController

--- User-Interface Multi-Chat_Application/src/Client_Side/controller/ClientLoginFormController.java
+++ User-Interface Multi-Chat_Application/src/Client_Side/controller/ClientLoginFormController.java
@@ -1,14 +1,43 @@
 package Client_Side.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ClientLoginFormController {
 
     public AnchorPane mainRoot;
     public TextField txtName;
 
-    public void continueBtnOnAction(ActionEvent actionEvent) {
+    public static String userName;
+
+    public static ArrayList<String> users = new ArrayList<>();
+
+    public void continueBtnOnAction(ActionEvent actionEvent) throws IOException {
+
+        userName = txtName.getText().trim();
+        boolean isAdd = false;
+        if(users.isEmpty()){
+            users.add(userName);
+            isAdd = true;
+        }
+
+        for(String s : users){
+            if (!s.equalsIgnoreCase(userName)) {
+                isAdd = true;
+                System.out.println(userName);
+                break;
+            }
+        }
+
+        if(isAdd){
+            this.mainRoot.getChildren().clear();
+            this.mainRoot.getChildren().add(FXMLLoader.load(this.getClass().getResource("../view/MessageEnvironmentForm.fxml")));
+        }
+
     }
 }
